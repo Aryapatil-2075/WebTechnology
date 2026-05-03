77547cb8e8d8bb34c9f6b8e8577d1ff5797e6321
Aryapatil-2075 <aryapatil2075@gmail.com>
2026-05-03 23:23:41 +0530

    Leave Management using Angular

--- classActivity/project/Leave-Management/myapp/src/app/app.routes.ts
+++ classActivity/project/Leave-Management/myapp/src/app/app.routes.ts
@@ -5,13 +5,12 @@ import { Dashboard } from '../components/dashboard/dashboard';
 import { ApplyLeave } from '../components/apply-leave/apply-leave';
 import { MyLeaves } from '../components/my-leaves/my-leaves';
 import { ManageLeaves } from '../components/manage-leaves/manage-leaves';
-import { RoleGuard } from '../Guards/role.guard';
 
 export const routes: Routes = [
   { path: '', component: Dashboard },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
-  { path: 'apply', component: ApplyLeave, canActivate: [RoleGuard], data: { role: 'employee' } },
-  { path: 'my', component: MyLeaves, canActivate: [RoleGuard], data: { role: 'employee' } },
-  { path: 'manage', component: ManageLeaves, canActivate: [RoleGuard], data: { role: 'admin' } }
+  { path: 'apply', component: ApplyLeave },
+  { path: 'my', component: MyLeaves },
+  { path: 'manage', component: ManageLeaves }
 ];
